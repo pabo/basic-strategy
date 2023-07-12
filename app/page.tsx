951f96efab62209f682b73f1d395f6f76cccc30c
pabo <brett.schellenberg@gmail.com>
2023-07-12 17:24:19 -0600

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,135 @@
-import Image from 'next/image'
+'use client';
+
+import { Fragment, ReactNode, SetStateAction, SyntheticEvent, useState } from 'react';
+import {
+    Action,
+    TableRow,
+    doubleAfterSplitChanges,
+    hardTableDefault,
+    soft17HardChanges,
+    soft17SoftChanges,
+    softTableDefault,
+    splitTableDefault,
+} from './data/tables';
+
+const actionColors: { [key in Action]: string } = {
+    [Action.Hit]: 'red',
+    [Action.Stand]: 'yellow',
+    [Action.Double]: 'blue',
+    [Action.Split]: 'green',
+    [Action.Surrender]: 'white',
+    [Action.None]: 'white',
+};
+interface actionChange {
+    playerIndex: number; // TODO: this is a little fragile
+    dealerUpCard: number;
+    actionIfEnabled: Action;
+    actionIfDisabled: Action;
+}
+
+const makeUpdates = (
+    table: TableRow[],
+    setTable: { (value: SetStateAction<TableRow[]>): void; (arg0: TableRow[]): void },
+    ruleEnabled: boolean,
+    actionChanges: actionChange[]
+) => {
+    const newTable = [...table];
+
+    for (const change of actionChanges) {
+        const { playerIndex, dealerUpCard, actionIfEnabled, actionIfDisabled } = change;
+
+        newTable[playerIndex].cells[dealerUpCard - 2] = ruleEnabled
+            ? actionIfEnabled
+            : actionIfDisabled;
+    }
+
+    setTable(newTable);
+};
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    const [softTable, setSoftTable] = useState(softTableDefault);
+    const [hardTable, setHardTable] = useState(hardTableDefault);
+    const [splitTable, setSplitTable] = useState(splitTableDefault);
+    const [doubleAfterSplit, setDoubleAfterSplit] = useState(true);
+    const [hitSoft17, setHitSoft17] = useState(true);
+
+    const handleDASChange = (e: SyntheticEvent) => {
+        makeUpdates(splitTable, setSplitTable, !doubleAfterSplit, doubleAfterSplitChanges);
+        setDoubleAfterSplit(x => !x);
+    };
+
+    const handleS17Change = (e: SyntheticEvent) => {
+        makeUpdates(hardTable, setHardTable, !hitSoft17, soft17HardChanges);
+        makeUpdates(softTable, setSoftTable, !hitSoft17, soft17SoftChanges);
+        setHitSoft17(x => !x);
+    };
+
+    return (
+        <div>
+            <h1>Blackjack Basic Strategy</h1>
+            <p>See how basic strategy changes by ruleset</p>
+
+            <div>
+                <label htmlFor="das">Double After Split</label>
+                <input
+                    id="das"
+                    type="checkbox"
+                    onChange={handleDASChange}
+                    checked={!!doubleAfterSplit}
+                />
+                <br />
+                <label htmlFor="s17">Dealer Hits Soft 17</label>
+                <input id="s17" type="checkbox" onChange={handleS17Change} checked={!!hitSoft17} />
+            </div>
+            {[hardTable, softTable, splitTable].map((table, index) => {
+                return (
+                    <Fragment key={index}>
+                        <Table>
+                            {table.map((row, rowIndex) => {
+                                return <Row header={row.header} cells={row.cells} key={rowIndex} />;
+                            })}
+                        </Table>
+                    </Fragment>
+                );
+            })}
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    );
 }
+
+const Cell = (props: { header?: string; action?: Action }) => {
+    const { header, action = Action.None } = props;
+
+    return (
+        <div
+            style={{
+                width: '25px',
+                height: '25px',
+                padding: '2px',
+                fontWeight: header ? 800 : 400,
+                backgroundColor: header ? 'white' : actionColors[action],
+                textAlign: 'center',
+            }}
+        >
+            {props.header ? props.header : props.action}
+        </div>
+    );
+};
+
+const Row = (props: { header: string; cells: Action[] }) => {
+    return (
+        <div style={{ display: 'flex' }}>
+            <Cell header={props.header} />
+            {props.cells.map((cell, index) => {
+                return <Cell action={cell} key={index} />;
+            })}
+        </div>
+    );
+};
+
+const Table = (props: { children: ReactNode }) => {
+    return (
+        <div style={{ float: 'left', display: 'flex', flexDirection: 'column' }}>
+            {props.children}
+        </div>
+    );
+};
